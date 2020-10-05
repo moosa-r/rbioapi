f9a16421286cd6871f6e0a19d5f2d654b2faba21
Moosa Rezwani <moosa.rezwani@gmail.com>
2020-10-05 04:31:12 +0330

    revised ensembl taxonomy functions.

--- R/ensembl_taxonomy.R
+++ R/ensembl_taxonomy.R
@@ -34,79 +34,57 @@ rba_ensembl_taxonomy_classification = function(taxon_id,
   return(final_output)
 }
 
-#' Search for a taxonomic term by its identifier or name
+#' Search for a taxonomic term by its identifier, name or non-scientific name
 #'
-#' @param taxon_id
 #' @param ...
 #' @param simple
+#' @param taxon
+#' @param non_scientific_name
 #'
 #' @return
 #' @export
 #'
 #' @examples
-rba_ensembl_taxonomy_id = function(taxon_id,
-                                   simple = FALSE,
-                                   ...) {
+rba_ensembl_taxonomy_search = function(taxon = NA,
+                                       non_scientific_name = NA,
+                                       simple = FALSE,
+                                       ...) {
   ## Load Global Options
   rba_ba_ext_args(...)
   ## Check User-input Arguments
-  rba_ba_args(cons = list(list(arg = "taxon_id",
+  rba_ba_args(cons = list(list(arg = "taxon",
                                class = c("numeric",
                                          "character")),
+                          list(arg = "non_scientific_name",
+                               class = "character"),
                           list(arg = "simple",
-                               class = "logical")))
+                               class = "logical")),
+              cond = list(list(quote(sum(!is.na(taxon), !is.na(non_scientific_name)) != 1),
+                               "You shoul provide one of 'taxon' or 'non_scientific_name'."),
+                          list(quote(simple == TRUE && is.na(taxon)),
+                               "You can only set 'simple' to 'TRUE' when providing 'taxon'")))
 
   v_msg("GET taxonomy/id/:id")
 
   ## Build GET API Request's query
   call_query = rba_ba_query(init = list(),
                             list("simple",
-                                 simple = TRUE,
+                                 simple == TRUE,
                                  "1"))
   ## Build Function-Specific Call
+  path_input = ifelse(!is.na(taxon),
+                      yes = paste0("taxonomy/id/", taxon),
+                      no = paste0("taxonomy/name/", non_scientific_name))
   input_call = rba_ba_httr(httr = "get",
                            url = rba_ba_stg("ensembl", "url"),
-                           path = paste0("taxonomy/id/",
-                                         taxon_id),
+                           path = path_input,
                            query = call_query,
                            accept = "application/json",
                            parser = "json->df",
-                           save_to = rba_ba_file("ensembl_taxonomy_id.json"))
+                           save_to = rba_ba_file("ensembl_taxonomy_search.json"))
 
   ## Call API
   final_output = rba_ba_skeleton(input_call)
   return(final_output)
 }
 
-#' Search for a taxonomic id by a non-scientific name
-#'
-#' @param ...
-#' @param name
-#'
-#' @return
-#' @export
-#'
-#' @examples
-rba_ensembl_taxonomy_name = function(name,
-                                     ...) {
-  ## Load Global Options
-  rba_ba_ext_args(...)
-  ## Check User-input Arguments
-  rba_ba_args(cons = list(list(arg = "name",
-                               class = "character")))
-
-  v_msg("GET taxonomy/name/:name")
-
-  ## Build Function-Specific Call
-  input_call = rba_ba_httr(httr = "get",
-                           url = rba_ba_stg("ensembl", "url"),
-                           path = paste0("taxonomy/name/",
-                                         name),
-                           accept = "application/json",
-                           parser = "json->df",
-                           save_to = rba_ba_file("ensembl_taxonomy_name.json"))
-
-  ## Call API
-  final_output = rba_ba_skeleton(input_call)
-  return(final_output)
-}
