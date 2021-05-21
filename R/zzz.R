36ce8a0c806e2b1a80337202524a4fec94ff9d00
Moosa Rezwani <moosa.rezwani@gmail.com>
2021-05-21 15:45:30 +0430

    Make the package description more concise

--- R/zzz.R
+++ R/zzz.R
@@ -1,24 +1,13 @@
-#' @title rbioapi: User-Friendly R Interface to Biologic Web Services' API
-#'
-#' @description Currently fully supports miEAA, PANTHER, Reactome, STRING,
-#'   and UniProt! The goal of rbioapi is to provide a user-friendly and
-#'   consistent interface to biological databases and services; It is
-#'   designed in a way that insulates the user from technicalities when
-#'   it comes to using API services and creates a unified and
-#'   easy-to-implement tool to connect to biological databases and services.
-#'   With rbioapi, You are not required to have any prior technical
-#'   knowledge. Just fill in a function's arguments and the rest is handled
-#'   for you. This an ongoing project. New databases and services will be
-#'   implemented periodically to gradually make rbioapi more comprehensive.
-#'   Feel free to suggest any databases or services you often use.
-#'
-#' @section Currently, rbioapi fully supports and Covers API resources of:
+#' @section Supported Database/Services:
 #' \itemize{
+#'   \item Enrichr
+#'   \item JASPAR
 #'   \item MiEAA
 #'   \item PANTHER
 #'   \item Reactome
 #'   \item STRING
-#'   \item UniProt}
+#'   \item UniProt
+#'   }
 #'
 #' @docType package
 #' @name rbioapi
